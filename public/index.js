2f5199c854652bcabe7a0abe0fc8f7ec38a7d7d3
David McCulloch <davidmcculloch98@gmail.com>
2024-01-25 10:30:52 +0000

    add dummy images, reduce imports using index.js, change page title

---
+++ public/index.js
@@ -0,0 +1,10 @@
+import web1 from "./web1.png";
+import web2 from "./web2.png";
+import web3 from "./web3.png";
+import web4 from "./web4.png";
+import web5 from "./web5.png";
+import web6 from "./web6.png";
+import sky from "./sky.png";
+import fdm from "./fdm.jpg";
+
+export { web1, web2, web3, web4, web5, web6, sky, fdm };
